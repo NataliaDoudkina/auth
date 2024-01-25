a78f33453143ffec6e6cdc4662757a97c0a3eca3
NataliaDoudkina <48611728+NataliaDoudkina@users.noreply.github.com>
2024-01-24 21:41:56 -0500

    Create myFunctions.js

---
+++ functions/myFunctions.js
@@ -0,0 +1,12 @@
+// functions/myFunction.js
+
+exports.handler = async (event, context) => {
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Headers': 'Content-Type',
+    },
+    body: JSON.stringify({ message: 'Hello from the serverless function' }),
+  };
+};
